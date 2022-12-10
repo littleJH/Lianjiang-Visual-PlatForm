3cc7a6e8b0e9d2cbbed33527c77702badaf9ee0c
zhangjiahao <2240046361@qq.com>
2022-12-10 23:19:52 +0800

    1.1.1

--- src/main.js
+++ src/main.js
@@ -5,17 +5,17 @@ import ElementUI from 'element-ui'
 import BaiduMap from 'vue-baidu-map'
 import * as echarts from 'echarts'
 import moment from 'moment'
-import store from './store'
 import 'github-markdown-css/github-markdown.css'
 
 Vue.config.productionTip = false
-Vue.use(BaiduMap, { ak: '' })
+Vue.use(BaiduMap, {
+  ak: ''
+})
 Vue.use(ElementUI)
 Vue.prototype.$echarts = echarts
 Vue.prototype.$moment = moment
 
 new Vue({
   router,
-  render: h => h(App),
-  store
+  render: h => h(App)
 }).$mount('#app')
--- src/store.js
+++
@@ -1,7 +0,0 @@
-import vuex from 'vuex'
-import Vue from 'vue'
-Vue.use(vuex)
-
-const store = new vuex.Store({})
-
-export default store
